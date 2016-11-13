1be79a420963c8121d88ed546bdd0868fcc43642
Alex Jorgenson <jorgy343@gmail.com>
2016-11-13 10:06:46 -0600

    First real stab at virtual memory.

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -1,11 +1,6 @@
-#include <string.h>
-#include <stdio.h>
-
 #include "standard.h"
 #include "mini-uart.h"
 #include "gpio.h"
-
-void main();
 
 void main()
 {
@@ -14,9 +9,57 @@ void main()
     
     MiniUartWriteString("Mini uart initialized\r\n");
 
-    asm("svc 123");
+    uint64_t tcr;
+    ReadSystemRegister(tcr_el1, tcr);
 
-    //printf("test");
+    // Pi3 supports a maximum of 1TiB of memory. The T0SZ value is
+    // then 64 - 40 = 24. This means that TTBR0 will point to a
+    // level two table instead of a level one.
+    tcr = SetBits64(tcr, 24, 0, 5);
+
+    // 64KiB granule size for TTBR0.
+    tcr = SetBits64(tcr, 1, 14, 15);
+
+    // IPA is 40 bits in width (1TiB).
+    tcr = SetBits64(tcr, 2, 32, 34);
+
+    // 1TiB address size limit for TTBR1 (same as TTBR0).
+    tcr = SetBits64(tcr, 24, 16, 21);
+
+    // 64KiB granule lsize for TTBR1.
+    tcr = SetBits64(tcr, 1, 30, 31);
+
+    MiniUartWriteHex64(tcr);
     MiniUartWriteString("\r\n");
+
+    WriteSystemRegister(tcr_el1, tcr);
+
+    // Setup the level 2 table.
+    volatile uint64_t* block0 = (uint64_t*)0x500000;
+    volatile uint64_t* block1 = block0 + 1;
+    volatile uint64_t* block2 = block0 + 2;
+    volatile uint64_t* block3 = block0 + 3;
+
+    *block0 = 0x00000000;
+    *block1 = 0x20000000;
+    *block2 = 0x40000000;
+    *block3 = 0x60000000;
+
+    *block0 |= 0x1;
+    *block1 |= 0x1;
+    *block2 |= 0x1;
+    *block3 |= 0x1;
+
+    uint64_t ttbr0 = (uint64_t)block0;
+    WriteSystemRegister(ttbr0_el1, ttbr0); // 80MiB mark.
+    
+    // Enable the MMU.
+    uint64_t sctlr;
+    ReadSystemRegister(sctlr_el1, sctlr);
+    
+    sctlr |= 0x1;
+    WriteSystemRegister(sctlr_el1, sctlr);
+
+    MiniUartWriteString("Virtual memory initialized\r\n");
     while (true);
 }
--- source/kernel/standard.h
+++ source/kernel/standard.h
@@ -6,9 +6,21 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include <stdlib.h>
-
+// Useful macros.
 #define bit(x) (1 << (x))
-#define align(x, y) ((x + y - 1) & ~(y - 1))
+#define align(x, y) ((x + y - 1) & ~(y - 1)) // Advances x to the next y boundary if necessary.
+
+#define BitRangeMask64(start, end) (((uint64_t)-1 >> (63 - end)) & ~(((uint64_t)1 << start) - (uint64_t)1))
+#define BitRangeMask32(start, end) (((uint32_t)-1 >> (31 - end)) & ~(((uint32_t)1 << start) - (uint32_t)1))
+#define BitRangeMask16(start, end) (((uint16_t)-1 >> (15 - end)) & ~(((uint16_t)1 << start) - (uint16_t)1))
+#define BitRangeMask8(start, end) (((uint8_t)-1 >> (7 - end)) & ~(((uint8_t)1 << start) - (uint8_t)1))
+
+#define SetBits64(x, y, start, end) ((x & ~BitRangeMask64(start, end)) | ((uint64_t)y << start))
+
+#define ReadRegister(reg, x) asm("mov %[_x], "#reg : [_x] "=r" (x))
+#define ReadSystemRegister(reg, x) asm("mrs %[_x], "#reg : [_x] "=r" (x))
+
+#define WriteRegister(reg, x) asm("mov "#reg", %[_x]" : [_x] "=r" (x))
+#define WriteSystemRegister(reg, x) asm("msr "#reg", %[_x]\n\t" "isb" : [_x] "=r" (x))
 
 #endif
